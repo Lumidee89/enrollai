0754fe5e24e3b7426a11b3e4d2e0cf30c81bec4c
Lumidee89 <97644187+Lumidee89@users.noreply.github.com>
2025-01-02 20:04:41 +0100

    update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,5 @@
 const Application = require('../models/applicationModel');
+const User = require('../models/User');
 
 exports.getAllApplications = async (req, res) => {
     try {
@@ -18,3 +19,14 @@ exports.getAllApplications = async (req, res) => {
       });
     }
   };
+
+  exports.getAllProviders = async (req, res) => {
+    try {
+      const providers = await User.find({ accountType: 'provider' }).select('-password');
+      res.status(200).json({ success: true, data: providers });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ success: false, message: 'Server error' });
+    }
+  };
+  
