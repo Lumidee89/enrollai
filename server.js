003d8b310ce216641303a687fe6b7a62bc31599f
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2025-01-23 11:56:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
-const caqhAuthRoutes = require('./routes/caqhauth');
-const applicationRoutes = require('./routes/applicationRoutes.js');
-const organizationRoutes = require('./routes/organizationRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const activityRoute = require('./routes/activityRoute');
-
+const caqhAuthRoutes = require("./routes/caqhauth");
+const applicationRoutes = require("./routes/applicationRoutes.js");
+const organizationRoutes = require("./routes/organizationRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const activityRoute = require("./routes/activityRoute");
 
 require("dotenv").config();
 
@@ -19,7 +18,8 @@ app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://enrollai.netlify.app"
+  "https://enrollai.netlify.app",
+  "http://localhost:5000",
 ];
 
 app.use(
@@ -39,11 +39,11 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use('/api/caqhauth', caqhAuthRoutes);
-app.use('/api/application', applicationRoutes);
-app.use('/api/organizations', organizationRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/activity', activityRoute);
+app.use("/api/caqhauth", caqhAuthRoutes);
+app.use("/api/application", applicationRoutes);
+app.use("/api/organizations", organizationRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/activity", activityRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
